cd3a6d973d166037d1331190429b4421356a9a9e
sofianbello <safian.bello@haw-hamburg.de>
2021-12-14 21:47:08 +0100

    Fixed Embed Parent on Landing

--- pages/imprint.vue
+++ pages/imprint.vue
@@ -2,19 +2,18 @@
     <div class='impressum'>
         <h1>Impressum</h1>
         <p>Angaben gemäß § 5 TMG</p>
-        <p>Max Muster
-        <br>Musterweg<br> 
-        12345 Musterstadt <br> 
+        <p>Brivals
+        <br>Kielerstr. 388<br> 
+        22525 Hamburg <br> 
         </p>
         <p> 
         <strong>Vertreten durch: </strong>
-        <br>Max Muster<br>
+        <br>Sofian Bello<br>
         </p>
         <p>
         <strong>Kontakt:</strong>
-        <br>Telefon: 01234-789456
-        <br>Fax: 1234-56789
-        <br>E-Mail: <a href='mailto:max@muster.de'>max@muster.de</a></br></p>
+        <br>Telefon: 0151-45078551
+        <br>E-Mail: <a href='mailto:team.brivals@gmail.com'>team.brivals@gmail.com</a></br></p>
         </p>
         <p><strong>Aufsichtsbehörde:</strong>
         <br>Musteraufsicht Musterstadt<br></p>
