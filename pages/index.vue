0077d7acb7ac2471dc07ba6575acbea7ec8a4609
sofianbello <safian.bello@haw-hamburg.de>
2021-12-28 16:20:22 +0100

    Updated Twitch-Embed-Parent

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
     <div class="twitch">
         <div class="twitch-video">
             <iframe
-            src="https://player.twitch.tv/?channel=brivals&enableExtensions=false&parent=localhost&player=popout&volume=0.3&autoplay=true" 
+            src="https://player.twitch.tv/?channel=brivals&enableExtensions=false&parent=www.brivals.tv&player=popout&volume=0.3&autoplay=true" 
             height="1080" 
             width="1920" 
             frameborder="0" 
@@ -12,7 +12,7 @@
         <div class="twitch-chat">
             <iframe
             frameborder="0" 
-            src="https://www.twitch.tv/embed/brivals/chat?darkpopout&parent=localhost"
+            src="https://www.twitch.tv/embed/brivals/chat?darkpopout&parent=www.brivals.tv"
             height="1080"
             width="450">
         </iframe>
