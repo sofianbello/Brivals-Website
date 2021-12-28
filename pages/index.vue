209b2f17e1e4f32241401c881484b64ce150d62c
sofianbello <safian.bello@haw-hamburg.de>
2021-12-28 16:24:44 +0100

    Fixing Twitch-Parents

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
     <div class="twitch">
         <div class="twitch-video">
             <iframe
-            src="https://player.twitch.tv/?channel=brivals&enableExtensions=false&parent=www.brivals.tv&player=popout&volume=0.3&autoplay=true" 
+            src="https://player.twitch.tv/?channel=brivals&enableExtensions=false&parent=www.brivals.tv&player=popout&volume=0.3&autoplay=true"
             height="1080" 
             width="1920" 
             frameborder="0" 
@@ -11,7 +11,7 @@
         </div>
         <div class="twitch-chat">
             <iframe
-            frameborder="0" 
+            frameborder="0" gi
             src="https://www.twitch.tv/embed/brivals/chat?darkpopout&parent=www.brivals.tv"
             height="1080"
             width="450">
